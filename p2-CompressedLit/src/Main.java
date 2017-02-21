2626b14d255ee26a6b7c480a0ea1ae50c936e6be
todc <cranet@uw.edu>
2017-02-21 13:32:45 -0800

    cleaned up main, print initial sizes

--- p2-CompressedLit/src/Main.java
+++ p2-CompressedLit/src/Main.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by todc on 2/18/2017.
@@ -7,56 +11,54 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        //Read contents of a text file into a string
+        //Create files
+        File inputText = new File("test.txt"); //File to be compressed
 
+        File codeFile = new File("codes.txt"); //File to store Huffman codes
+        codeFile.createNewFile();
+
+        File compressedFile = new File("compressed.txt"); //Final compressed file
+        compressedFile.createNewFile();
+
+        //Read contents of inputText file into a string
         StringBuilder sb = new StringBuilder();
         String line;
 
         try {
             //Fields
-            FileReader fr = new FileReader("test.txt");
-            BufferedReader bs = new BufferedReader(fr);
+            FileReader fr = new FileReader(inputText);
+            BufferedReader br = new BufferedReader(fr);
 
-            while((line = bs.readLine()) != null) {
+            while((line = br.readLine()) != null) {
                 sb.append(line);
                 sb.append((char) 10); //Account for lines
-                //System.out.println(line);
             }
-            bs.close();
+            br.close();
         } finally{}
-        //System.out.println(sb.toString());
 
         //Pass string to CodingTree (generate map of codes)
         CodingTree a = new CodingTree(sb.toString());
 
-
         //Output codes to a text file
 
-        //System.out.println(a.codes);
-        //System.out.println(a.bits);
-
-        //Create files
-        File codeFile = new File("codes.txt");
-        File bitFile = new File("compressed.txt");
-
-        codeFile.createNewFile();
-        bitFile.createNewFile();
-
-        //Write codes
+        //Write Huffman codes
         FileWriter writer = new FileWriter(codeFile);
         writer.write(a.codes.toString());
 
         writer.flush();
         writer.close();
 
-        //Write compressed
-        writer = new FileWriter(bitFile);
+        //Write compressed file
+        writer = new FileWriter(compressedFile);
         writer.write(a.bits);
 
         writer.flush();
         writer.close();
 
         //Display the size of the compressed text, compression, and run time statistics
+        System.out.println(compressedFile.length());
+        System.out.println(codeFile.length());
+        System.out.println(inputText.length());
 
 
     }
