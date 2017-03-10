1e76318cae4993c79f55c8d2f1db8836ab0630ae
cranet <cranet@uw.edu>
2017-03-10 00:33:46 -0800

    Cleaned up and comments

--- p3-Graphs/src/MyGraph.java
+++ p3-Graphs/src/MyGraph.java
@@ -9,14 +9,12 @@ import java.util.Set;
  * in the graph.
  */
 public class MyGraph implements Graph {
-	// you will need some private fields to represent the graph
-	// you are also likely to want some private helper methods
 
-	// YOUR CODE HERE
-    //private Set<Edge> edges;
+    //Graph representations
 	private HashMap<Vertex, Set<Edge>> map;
 	private HashMap<String, Vertex> vertMap;
 	private HashMap<String, Set<Edge>> edgeMap;
+
 	private Collection<Vertex> vertex;
 	private Collection<Edge> edge;
 
@@ -24,37 +22,22 @@ public class MyGraph implements Graph {
 	 * Creates a MyGraph object with the given collection of vertices and the
 	 * given collection of edges.
 	 * 
-	 * @param v
-	 *            a collection of the vertices in this graph
-	 * @param e
-	 *            a collection of the edges in this graph
+	 * @param v a collection of the vertices in this graph
+	 * @param e a collection of the edges in this graph
 	 */
 	public MyGraph(Collection<Vertex> v, Collection<Edge> e) {
-
-		// YOUR CODE HERE
-        //System.out.println(v);
-        //System.out.println(e);
-        vertex = v;
-        edge = e;
+	    //Fields
         map = new HashMap<>();
         vertMap = new HashMap<>();
         edgeMap = new HashMap<>();
+        vertex = v;
+        edge = e;
 
-        //edges = new HashSet<>();
-        //graph = new HashMap<>();
-
-        //Call helper method to check edges
+        //Check edges for exceptions
         checkEdges();
 
-        //Build adjacent map
-        buildMap();
-
-        //TEST
-        //for(Vertex vtest : vertex) {
-            //System.out.println("vtes t   : " + vtest);
-            //System.out.println(adjacentVertices(vtest));
-        //}
-
+        //Build adjacency list graph
+        buildGraph();
 	}
 
     /**
@@ -63,7 +46,6 @@ public class MyGraph implements Graph {
      * Checks for repeated directed edges with different weights
      */
 	private void checkEdges() {
-
         for(Edge e : edge) {
             //Check that all edges connect to existing vertices
             if(!vertex.contains(e.getSource()) || !vertex.contains(e.getDestination())) {
@@ -89,8 +71,10 @@ public class MyGraph implements Graph {
     /**
      * Builds the graph using a HashMap
      * Keys = Vertices, Values = HashSet of connected edges
+     *
+     * Builds two extra separate HashMaps (vertMap and edgeMap) for use with Djikstra
      */
-    private void buildMap() {
+    private void buildGraph() {
         for(Vertex v : vertex) {
             Set<Edge> edges = new HashSet<>();
             for(Edge e : edge) {
@@ -98,12 +82,10 @@ public class MyGraph implements Graph {
                     edges.add(e);
                 }
             }
+            map.put(v, edges);
             vertMap.put(v.getLabel(), v);
             edgeMap.put(v.getLabel(), edges);
-            map.put(v, edges);
-
         }
-        System.out.println(map.toString());
     }
 
 	/**
@@ -113,11 +95,7 @@ public class MyGraph implements Graph {
 	 */
 	@Override
 	public Collection<Vertex> vertices() {
-
-		// YOUR CODE HERE
-
         return vertex;
-
 	}
 
 	/**
@@ -127,11 +105,7 @@ public class MyGraph implements Graph {
 	 */
 	@Override
 	public Collection<Edge> edges() {
-
-		// YOUR CODE HERE
-
         return edge;
-
 	}
 
 	/**
@@ -139,26 +113,19 @@ public class MyGraph implements Graph {
 	 * set of all vertices w where edges v -> w exist in the graph. Return an
 	 * empty collection if there are no adjacent vertices.
 	 * 
-	 * @param v
-	 *            one of the vertices in the graph
+	 * @param v one of the vertices in the graph
 	 * @return an iterable collection of vertices adjacent to v in the graph
-	 * @throws IllegalArgumentException
-	 *             if v does not exist.
+	 * @throws IllegalArgumentException if v does not exist.
 	 */
 	@Override
 	public Collection<Vertex> adjacentVertices(Vertex v) {
-
-
-		// YOUR CODE HERE
         Collection<Vertex> temp = new HashSet<>();
-        //for(Map.Entry<Vertex, Edge> entry : map.entrySet())
 
         for(Edge e : edge) {
             if(e.getDestination().equals(v)) {
                 temp.add(e.getSource());
             }
         }
-
         return temp;
 	}
 
@@ -166,19 +133,14 @@ public class MyGraph implements Graph {
 	 * Test whether vertex b is adjacent to vertex a (i.e. a -> b) in a directed
 	 * graph. Assumes that we do not have negative cost edges in the graph.
 	 * 
-	 * @param a
-	 *            one vertex
-	 * @param b
-	 *            another vertex
+	 * @param a one vertex
+	 * @param b another vertex
 	 * @return cost of edge if there is a directed edge from a to b in the
 	 *         graph, return -1 otherwise.
-	 * @throws IllegalArgumentException
-	 *             if a or b do not exist.
+	 * @throws IllegalArgumentException if a or b do not exist.
 	 */
 	@Override
 	public int edgeCost(Vertex a, Vertex b) {
-
-		// YOUR CODE HERE
         for(Edge e : edge) {
             if(e.getSource().equals(a) && e.getDestination().equals(b)) {
                 return e.getWeight();
@@ -189,18 +151,15 @@ public class MyGraph implements Graph {
 
 	/**
 	 * Returns the shortest path from a to b in the graph, or null if there is
-	 * no such path. Assumes all edge weights are nonnegative. Uses Dijkstra's
+	 * no such path. Assumes all edge weights are non-negative. Uses Dijkstra's
 	 * algorithm.
 	 * 
-	 * @param a
-	 *            the starting vertex
-	 * @param b
-	 *            the destination vertex
+	 * @param a the starting vertex
+	 * @param b the destination vertex
 	 * @return a Path where the vertices indicate the path from a to b in order
 	 *         and contains a (first) and b (last) and the cost is the cost of
 	 *         the path. Returns null if b is not reachable from a.
-	 * @throws IllegalArgumentException
-	 *             if a or b does not exist.
+	 * @throws IllegalArgumentException if a or b does not exist.
 	 */
 	public Path shortestPath(Vertex a, Vertex b) {
 
@@ -215,219 +174,37 @@ public class MyGraph implements Graph {
         appear on the path.
          */
 
-
         /////////////
-
-        //Known vertices
-        //Map<String, Set<Edge>> known = new HashMap<>();
-
-        //Unknown vertices
-        //Set<Vertex> unknown = new HashSet<>();
-
-//        //Set infinity and false
-//        for(Vertex v : map.keySet()) {
-//            v.setCost(Integer.MAX_VALUE);
-//            v.setKnown(false);
-//            unknown.add(v);
-//        }
-
-
-
-        //Edges
-        //System.out.println(map.values().toString());
-//        Set<Edge> edges = new HashSet<>();
-//        //List<Edge> edges = new ArrayList<>();
-//        for(Set<Edge> setE : map.values()) {
-//            for(Edge e : setE) {
-//                edges.add(e);
-//            }
-//        }
-
-
-       // System.out.println(edges.toString());
-
 
         //Set source cost to zero
         a.setCost(0);
         Vertex current = a;
-        int i = 0;
+
+        //Iterate until target (b) is found
         while(!current.equals(b)) {
 
-            //System.out.println("current: " + current.toString());
-            //System.out.println("current cost: " + current.getCost());
-
-
-            //Find vertex with lowest cost
+            //Find vertex with lowest cost, set current cost
             int tempCost = Integer.MAX_VALUE;
-
             for(Vertex v : vertMap.values()) {
-                //System.out.println("vlabel: " + v.getLabel() + " cost: " + v.getCost());
                 if(v.getCost() < tempCost && !v.isKnown()) {
                     tempCost = v.getCost();
                     current = v;
                 }
             }
 
-            System.out.println("current: " + current.toString());
-            System.out.println("current cost: " + current.getCost());
-
-            //Set known
-            //current.setKnown(true);
+            //Set known vertex
             vertMap.get(current.getLabel()).setKnown(true);
-            //known.add(current);
-            //unknown.remove(current);
-            //System.out.println(current.toString());
 
             //Find shortest path from current
-            //int temp = Integer.MAX_VALUE;
             for(Edge e : edgeMap.get(current.getLabel())) {
-                //System.out.println(e.toString());
 
-                //Check for correct source
-                //if(e.getSource().toString().equals(current.getLabel())) {
-                //if(e.getSource().equals(current)) {
-
-
-                //Setting cost if less than previous cost
-//                if(e.getDestination().getCost() > current.getCost() + e.getWeight()) {
-//
-//                    e.getDestination().setCost(current.getCost() + e.getWeight());
-//                }
-
+                //If destination cost is greater than current cost, set cost to current cost + edge weight
                 if(vertMap.get(e.getDestination().getLabel()).getCost() > current.getCost() + e.getWeight()) {
-                    //System.out.println("before cost: " + vertMap.get(e.getDestination().getLabel()).getCost());
                     vertMap.get(e.getDestination().getLabel()).setCost(current.getCost() + e.getWeight());
-                    //System.out.println("after cost: " + vertMap.get(e.getDestination().getLabel()).getCost());
                 }
-
-                    //Get shortest weight
-//                    if(e.getWeight() < temp) {
-//                        temp = e.getWeight();
-//                        //System.out.println("weight: " + temp);
-//                    }
-                //}
-                //Set cost of current vertex
-                //current.setCost(temp);
             }
-
-            //Add vertex to known
-            //known.add(current);
-
-            i++;
-
-
         }
-
-
         //REMOVE
         return null;
-
     }
-
-
-
-
-        ////////////
-
-
-        /*
-        //Graph = map
-        Vertex current = a;
-        Vertex tempKey = current;
-        Set<Edge> tempValue = map.get(current);
-        tempKey.setCost(0);
-        map.remove(current);
-        map.put(tempKey, tempValue);
-
-        while(!current.equals(b)) {
-
-            int tempCost = Integer.MAX_VALUE;
-            for(Map.Entry<Vertex, Set<Edge>> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + " " + entry.getKey().isKnown());
-                if (entry.getKey().getCost() < tempCost && !entry.getKey().isKnown()) {
-                    tempCost = entry.getKey().getCost();
-                    current = entry.getKey();
-                    //entry.se
-                    //entry.getKey().setKnown(true);
-                }
-
-                //System.out.println("Entry cost: " + entry.getKey().getCost());
-            }
-            System.out.println("Current: " + current + ", Cost: " + current.getCost());
-
-//            current.setKnown(true);
-            tempKey = current;
-            tempValue = map.get(current);
-            tempKey.setKnown(true);
-            map.remove(current);
-            map.put(tempKey, tempValue);
-            current = tempKey;
-
-            for(Edge e : map.get(current)) {
-                //System.out.println(e.getSource() + " " + e.getDestination());
-                if(!e.getDestination().isKnown()) {
-
-                    if(current.getCost() + e.getWeight() < e.getDestination().getCost()) {
-
-                        tempKey = e.getDestination();
-                        tempValue = map.get(e.getDestination());
-                        tempKey.setCost(current.getCost() + e.getWeight());
-                        map.remove(e.getDestination());
-                        map.put(tempKey, tempValue);
-                        //System.out.print(current + "->");
-                    }
-                }
-            }
-        }
-        /*
-
-        /*
-        while(not all nodes are known) {
-            b = find unknown node with smallest cost
-            b.known = true
-            for each edge (b,a) in G
-            if(!a.known)
-                if(b.cost + weight((b,a)) < a.cost){
-                a.cost = b.cost + weight((b,a))
-                a.path = b
-            }
-        }
-        /*
-
-
-        /*
-
-        //Unvisited vertices
-        PriorityQueue<Vertex> pq = new PriorityQueue<>();
-        //Temp set
-        Collection<Vertex> temp = new HashSet<>();
-
-        //Set source cost to 0, add to set
-        a.setCost(0);
-        temp.add(a);
-
-        for(Map.Entry<Vertex, Set<Edge>> entry : map.entrySet()) {
-            while(!entry.getKey().isKnown()) {
-                System.out.println("entry value" + entry.getValue());
-                Edge nextNode = null;
-                int weight = Integer.MAX_VALUE;
-                for(Edge e : entry.getValue()) {
-                    if(weight > e.getWeight()) {
-                        weight = e.getWeight();
-                        nextNode = e;
-                    }
-                }
-                System.out.println("lowest weight: " + weight);
-                nextNode.getSource().setKnown(true);
-            }
-
-        }
-        */
-
-        /*
-
-        return null;
-	}
-	*/
-
 }
